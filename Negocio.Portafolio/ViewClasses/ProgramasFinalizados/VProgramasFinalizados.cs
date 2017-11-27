e468bd897475391100e1165ac6f8caeeb6ed1d87
Diego Galvez <d.galvezs@alumnos.duoc.cl>
2017-11-26 23:03:39 -0300

    Se agrego formato correo HTML Se agrego Vistas de Programas finalizados

---
+++ Negocio.Portafolio/ViewClasses/ProgramasFinalizados/VProgramasFinalizados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Portafolio.ViewClasses.ProgramasFinalizados
+{
+    public class VProgramasFinalizados
+    {
+        public int IdPrograma { get; set; }
+        public string NombrePrograma { get; set; }
+        public string NombreCEL { get; set; }
+        public string NombreEncargadoCEL { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaTermino { get; set; }
+    }
+}
---
+++ Negocio.Portafolio/ViewClasses/ProgramasFinalizados/VProgramasFinalizadosCollection.cs
@@ -0,0 +1,59 @@
+﻿using DALC.Portafolio;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Portafolio.ViewClasses.ProgramasFinalizados
+{
+    public class VProgramasFinalizadosCollection : List<VProgramasFinalizados>
+    {
+        public VProgramasFinalizadosCollection() { }
+
+        public VProgramasFinalizadosCollection(string json)
+        {
+            VProgramasFinalizadosCollection list = JsonConvert.DeserializeObject<VProgramasFinalizadosCollection>(json);
+
+            this.AddRange(list);
+        }
+
+
+        public VProgramasFinalizadosCollection LeerProgramasFinalizados()
+        {
+            var listaVista = new EntitiesCEM().VISTA_PROGRAMAS_FINALIZADOS;
+            return GenerarListado(listaVista.ToList());
+        }
+
+
+        private VProgramasFinalizadosCollection GenerarListado(List<VISTA_PROGRAMAS_FINALIZADOS> listaDALC)
+        {
+            VProgramasFinalizadosCollection listaBC = new VProgramasFinalizadosCollection();
+
+            foreach (var item in listaDALC)
+            {
+                VProgramasFinalizados programa = new VProgramasFinalizados();
+
+                programa.IdPrograma = item.ID_PROGRAMA;
+                programa.NombrePrograma = item.NOMBRE_PROGRAMA;
+                programa.NombreCEL= item.NOMBRE_CEL;
+                programa.NombreEncargadoCEL = item.NOMBRE_ENCARGADO_CEL;
+                programa.Pais = item.NOM_PAIS;
+                programa.Ciudad = item.NOM_CIUDAD;
+                programa.FechaInicio = item.FECHA_INICIO;
+                programa.FechaTermino = item.FECHA_TERMINO;
+
+                listaBC.Add(programa);
+            }
+            return listaBC;
+        }
+
+        //metodo que serializa la coleccion de Notas de Programa 
+        public string Serializar()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
